94b96ef05dcbb717aba1d033e98c6c3afb853e41
Phillip Weinberg <weinbe58@gmail.com>
2023-04-19 16:18:32 -0400

    convert task_results IR into a list of probabilties conditioned on pre and post sequence.

--- src/quera_ahs_utils/quera_ir/task_results.py
+++ src/quera_ahs_utils/quera_ir/task_results.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from pydantic import BaseModel, conlist, conint
+from typing import Dict, Tuple
 
 __all__ = [
     "QuEraTaskResults",    
@@ -23,8 +24,33 @@ class QuEraShotResult(BaseModel):
     pre_sequence: conlist(conint(ge=0, le=1), min_items=0) = []
     post_sequence: conlist(conint(ge=0, le=1), min_items=0) = []
 
+class TaskProbabilities(BaseModel):
+    probabilities: Dict[Tuple[str,str], float]
+
 class QuEraTaskResults(BaseModel):
     task_status: QuEraTaskStatusCode = QuEraTaskStatusCode.Failed
     shot_outputs: conlist(QuEraShotResult, min_items=0) = []
     
+    def export_as_probabilties(self) -> TaskProbabilities:
+        """converts from shot results to probabilities
 
+        Returns:
+            TaskProbabilities: The task results as probabilties
+        """
+        probabilities = dict()
+        n_shots = len(self.shot_outputs)
+        for shot_result in self.shot_outputs:
+            
+            pre_sequence_str = "".join(
+                str(bit) for bit in shot_result.pre_sequence
+            )
+            
+            post_sequence_str = "".join(
+                str(bit) for bit in shot_result.post_sequence
+            )
+            
+            configuration = (pre_sequence_str,post_sequence_str)
+            probabilities[configuration] = \
+                probabilities.get(configuration, 0) + 1.0/n_shots
+                
+        return TaskProbabilities(probabilities)
