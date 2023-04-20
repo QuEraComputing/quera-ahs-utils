61d7436b95e495401858fe11029ec5e0853d4df0
Phillip Weinberg <weinbe58@gmail.com>
2023-04-19 21:37:59 -0400

    adding task simulation.

--- src/quera_ahs_utils/quera_ir/task_results.py
+++ src/quera_ahs_utils/quera_ir/task_results.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from pydantic import BaseModel, conlist, conint
-from typing import List, Tuple
+from typing import Callable, Optional, List, Tuple
+import numpy as np
 
 __all__ = [
     "QuEraTaskResults",  
@@ -27,20 +28,49 @@ class QuEraShotResult(BaseModel):
 
 class TaskProbabilities(BaseModel):
     probabilities: List[Tuple[Tuple[str,str],float]]
+    
+    def simulate_task_result(self, shots = 1):
+        bit_strings, probabilities = zip(*self.probabilities)
+        
+        indices = np.random.choice(
+            len(probabilities), 
+            p=probabilities,
+            size=shots
+        )
+        shot_outputs = []
+        for index in indices:
+            pre_string, post_string = bit_strings[index]
+            pre_sequence = [int(bit) for bit in pre_string]
+            post_sequence = [int(bit) for bit in post_string]
+            
+            shot_outputs.append(
+                QuEraShotResult(
+                    shot_status = QuEraShotStatusCode.Completed,
+                    pre_sequence = pre_sequence,
+                    post_sequence = post_sequence
+                )
+            )
+        
+        return QuEraTaskResults(
+            task_status=QuEraTaskStatusCode.Completed,
+            shot_outputs=shot_outputs
+        )
 
 class QuEraTaskResults(BaseModel):
     task_status: QuEraTaskStatusCode = QuEraTaskStatusCode.Failed
     shot_outputs: conlist(QuEraShotResult, min_items=0) = []
     
-    def export_as_probabilties(self) -> TaskProbabilities:
+    def export_as_probabilties(self, post_process=False) -> TaskProbabilities:
         """converts from shot results to probabilities
 
         Returns:
             TaskProbabilities: The task results as probabilties
         """
         probabilities = dict()
-        n_shots = len(self.shot_outputs)
+        n = 0
         for shot_result in self.shot_outputs:
+            if any(bit==0 for bit in shot_result.pre_sequence):
+                continue
             
             pre_sequence_str = "".join(
                 str(bit) for bit in shot_result.pre_sequence
@@ -51,7 +81,29 @@ class QuEraTaskResults(BaseModel):
             )
             
             configuration = (pre_sequence_str,post_sequence_str)
+            # iterative average
             probabilities[configuration] = \
-                probabilities.get(configuration, 0) + 1.0/n_shots
+                ((n + 1.0) * probabilities.get(configuration, 0) + 1.0)/n
                 
+            n += 1
+            
         return TaskProbabilities(list(probabilities.items()))
+    
+    def post_process(self, keep_shot_result: Optional[Callable] = None):
+        
+        if keep_shot_result == None:
+            keep_shot_result = \
+                lambda shot_result: \
+                    any(bit==0 for bit in shot_result.pre_sequence)
+        
+        shot_outputs = []
+        for shot_result in self.shot_outputs:
+            if not keep_shot_result(shot_result):
+                continue
+            
+            shot_outputs.append(shot_result)
+            
+        return QuEraTaskResults(
+            task_status=self.task_status,
+            shot_outputs=shot_outputs
+        )
